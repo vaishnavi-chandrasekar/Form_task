914a5ac11ba5f61a9adfc9efd3228e986205a7a7
vaishnavi <vaishnavichandrasekar123@gmail.com>
2022-04-19 17:06:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { useFormik } from "formik";
 
 function App() {
+  const [value, ResetForm] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      FirstName: "",
+      LastName: "",
+      Email: "",
+      PhoneNumber: "",
+    },
+
+    onSubmit: async (values) => {
+      try {
+        await fetch("https://61c4cef8f1af4a0017d997ea.mockapi.io/users", {
+          method: "POST",
+          body: JSON.stringify(values),
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+        alert("data saved");
+        ResetForm("")
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let items = await fetch(
+          "https://61c4cef8f1af4a0017d997ea.mockapi.io/users"
+        );
+        let userdata = await items.json();
+        setUsers(userdata);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background">
+        <div className="form">
+          <form onSubmit={formik.handleSubmit}>
+            <h1>Info</h1>
+            <input
+              type="text"
+              placeholder="First Name"
+              name="FirstName"
+              class="form-control"
+              onChange={formik.handleChange}
+              value={formik.values.value}
+              required
+            ></input>
+            <br />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="LastName"
+              class="form-control"
+              onChange={formik.handleChange}
+              value={formik.values.value}
+              required
+            ></input>
+            <br />
+            <input
+              type="Email"
+              placeholder="Email"
+              name="Email"
+              class="form-control"
+              onChange={formik.handleChange}
+              value={formik.values.value}
+              required
+            ></input>
+            <br />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              name="PhoneNumber"
+              class="form-control"
+              onChange={formik.handleChange}
+              value={formik.values.value}
+              required
+            ></input>
+            <br />
+            <button type="submit">Submit</button>
+            <br />
+
+            <button type="reset" onClick={() => ResetForm(() => "")}>
+              Reset
+            </button>
+          </form>
+        </div>
+
+        <div className="table">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{user.FirstName}</td>
+                    <td>{user.Email}</td>
+                    <td>{user.PhoneNumber}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
 
